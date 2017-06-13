d508e1495734d3d00b634c34fef26fc504f89534
Cademeakin <lazygamers98@gmail.com>
2017-06-14 00:58:56 +0100

    P2 and P3

---
+++ P3 WSS/file_03.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<?php
+
+$myFile=fopen("mydata.txt","r") or exit("Can't open file!");
+
+// Read and display each line of text until the end of the file is reached
+
+while(!feof($myFile))
+{
+    echo fgets($myFile) . "<br>";
+}
+
+fclose($myFile);
+
+?>
+
+</body>
+</html>
---
+++ P3 WSS/files_01.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+    
+    <?php
+    $myFile=fopen("mydata.txt","r") or exit("Can't open file!");
+fclose($myFile);
+?>
+
+</body>
+</html>
